2d2030c5a8f8ac8c8a6e6461518d46c7fa15a54a
Khoi Hoang <khoi.hoang@bouvet.no>
2022-08-22 14:15:05 +0200

    add: ✨ Protecting Routes (Auth Guard)

--- src/router.js
+++ src/router.js
@@ -1,18 +1,36 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import WelcomePage from './components/welcome/welcome.vue'
-import DashboardPage from './components/dashboard/dashboard.vue'
-import SignupPage from './components/auth/signup.vue'
-import SigninPage from './components/auth/signin.vue'
+import store from './store';
 
-Vue.use(VueRouter)
+import WelcomePage from './components/welcome/welcome.vue';
+import DashboardPage from './components/dashboard/dashboard.vue';
+import SignupPage from './components/auth/signup.vue';
+import SigninPage from './components/auth/signin.vue';
+
+Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: WelcomePage },
   { path: '/signup', component: SignupPage },
   { path: '/signin', component: SigninPage },
-  { path: '/dashboard', component: DashboardPage }
-]
+  {
+    path: '/dashboard',
+    component: DashboardPage,
+    beforeEnter(to, from, next) {
+      const isAuth = store.state.idToken;
 
-export default new VueRouter({mode: 'history', routes})
+      console.log(isAuth);
+
+      if (isAuth) {
+        next();
+      }
+
+      if (!isAuth) {
+        next('/signin');
+      }
+    },
+  },
+];
+
+export default new VueRouter({ mode: 'history', routes });
